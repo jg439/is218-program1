4e42746f322b38d110bef267b3f63b65dec420cb
Julia <jg439@njit.edu>
2016-09-29 13:36:45 -0400

    more code

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
-
+//this is how you print text in php
 echo 'hello world';
-
+ //how to store value in a variable
+$myvar = 'Some text i want to store in a variable';
+echo '<br>';
+//difference between single ' and " quotes
+echo '$myvar';
+echo '<br>';
+echo "$myvar";
 
 
 ?>
