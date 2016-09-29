fd696475f682ef56f27e0969c0bf1828e1d7dc1c
Julia <jg439@njit.edu>
2016-09-29 14:25:48 -0400

    objects and methods

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ echo '$myvar';
 echo '<br>';
 echo "$myvar";
 
-//this is an example of php arrays
+//this is an example of php arrays. adding values.
 
 $myarray = array();
 $myarray[] = 'some value 1';
@@ -22,4 +22,32 @@ $myAssoc = array('value1' => $myarray, 'value2' => $myarray);
 
 print_r($myAssoc);
 
+
+//this is how you print a specific value on your array
+print_r($myAssoc['value1']);
+
+
+//this is how you definde a class
+class myclass{
+	public $myPublic;
+	private $myPrivate;
+	protected $myProtected;
+        //this is the internal reference to an object within a lass, the arrow points to the property or method being accessed within the object.
+	public function __construct(){
+	$this->myPublic =1;
+	$this->myPrivate =2;
+	$this->myProtected =3;
+		//This is how you call a methid insude an object and pass a value.
+		$this->sayHello('Julia');
+	}
+	
+	public function sayHello($name){
+
+		echo 'Hello' . $name . "<br>";
+	}
+
+}
+//this is how you instantiate a new class object
+$obj = new myclass;
+print_r($obj);
 ?>
