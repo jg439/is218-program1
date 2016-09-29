4024378f21caeb6bb1c4c16a3fd7184a2b8f3160
Julia <jg439@njit.edu>
2016-09-29 13:50:06 -0400

    added array info

--- index.php
+++ index.php
@@ -9,5 +9,17 @@ echo '$myvar';
 echo '<br>';
 echo "$myvar";
 
+//this is an example of php arrays
+
+$myarray = array();
+$myarray[] = 'some value 1';
+$myarray[] = 'some value 2';
+$myarray[] = 'some value 3';
+
+print_r($myarray);
+//example of associate array, also example of a nested array 2 arrays.
+$myAssoc = array('value1' => $myarray, 'value2' => $myarray);
+
+print_r($myAssoc);
 
 ?>
